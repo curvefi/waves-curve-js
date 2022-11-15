82d3723a38001f13b5bbededd393ffbda9a5784e
macket <macket24@proton.me>
2022-11-15 16:38:49 +0400

    feat: mainnet

--- src/constants/coins.ts
+++ src/constants/coins.ts
@@ -1,28 +1,23 @@
-// TESTNET
+// MAINNET
 export const COINS = {
-    "waves" : "WAVES",
-    "crv" : "CAcjv2M2VnJqoKR5aZodRv5o2PHTbFxGcUEHSwpa3Kcw",
+    waves: 'WAVES',
+    crv: '3KhNcHo4We1G5EWps7b1e5DTdLgWDzctc8S6ynu37KAb',
 
-    "usdn" : "h67Gp9FQDa6tVFXbfhBrkKWzLvAygHk4sPG7WySHgnA",
-    "usdt" : "2byPKMNSe4QVuUm2CEPjcVkPSDxQysX5CaJQXxKnJZPb",
-    "usdc" : "E3HWLmLLNJ8vC3U3SPfxLdoBbzmnZQmToopicQwD91yN",
-    "busd" : "9MZwABiVKKb1KebbJET7941wJqZs9hG8d9Reza1QTdt3",
-    "wdai" : "EDPRXujFFy5DJySGFbj8ckB9WCi9pF2rLqab66jrfvz",
-    "east" : "GjscxB4McTpst8SbhgFbY8UDMSdswuNmPYe4kTNsQjQb",
+    usdt: '34N9YcEETLWn93qYQ64EsP1x89tSruJU44RrEMSXXEPJ',
+    usdc: '6XtHjpXbs9RRJP2Sr9GUyVqzACcby9TkThHXnjVC5CDJ',
+    busd: '8DLiYZjo3UUaRBTHU7Ayoqg4ihwb6YH1AfXrrhdjQ7K1',
+    dai: '8zUYbdB8Q6mDhpcXYv52ji8ycfj4SDX4gJXS7YY3dA4R',
 };
 
-// CUSTOM NETWORK
+// // TESTNET
 // export const COINS = {
 //     "waves" : "WAVES",
-//     "usdn" : "5eg2eUvmgBftgsx3NZuCgk5q7195zx7VxNoNWatGS3HD",
-//     "usdt" : "DJEYQHGZ2LZzr37a5NK7BrYY3XKdAVXAFvaML9SGV7Do",
-//     "usdc" : "CPsgiCdE97iKU7zW9roH8C3mJ3yHMhGc7bkvTatp3R4L",
-//     "crv" : "GGmeY8wGA8y2apHrxLrUXCgF8REBtg5WGwgYS4j2FMJ2",
-// }
-
-// MAINNET
-// coin_addresses: [
-//     'DG2xFkPdDwKUoBkzGAhQtLpSGzfXLiCYPEzeKH2Ad24p', // USDN
-//     '34N9YcEETLWn93qYQ64EsP1x89tSruJU44RrEMSXXEPJ', // USDT
-//     '6XtHjpXbs9RRJP2Sr9GUyVqzACcby9TkThHXnjVC5CDJ', // USDC
-// ],
+//     "crv" : "CAcjv2M2VnJqoKR5aZodRv5o2PHTbFxGcUEHSwpa3Kcw",
+//
+//     "usdn" : "h67Gp9FQDa6tVFXbfhBrkKWzLvAygHk4sPG7WySHgnA",
+//     "usdt" : "2byPKMNSe4QVuUm2CEPjcVkPSDxQysX5CaJQXxKnJZPb",
+//     "usdc" : "E3HWLmLLNJ8vC3U3SPfxLdoBbzmnZQmToopicQwD91yN",
+//     "busd" : "9MZwABiVKKb1KebbJET7941wJqZs9hG8d9Reza1QTdt3",
+//     "wdai" : "EDPRXujFFy5DJySGFbj8ckB9WCi9pF2rLqab66jrfvz",
+//     "east" : "GjscxB4McTpst8SbhgFbY8UDMSdswuNmPYe4kTNsQjQb",
+// };
--- src/constants/poolsData.ts
+++ src/constants/poolsData.ts
@@ -1,27 +1,25 @@
 import { IDict, IPoolData } from '../interfaces';
 
 export const POOLS_DATA: IDict<IPoolData> = {
-    // TESTNET
-    "waves3pool" : {
-        "name" : "waves3pool",
-        "full_name" : "waves3pool",
-        "symbol" : "waves3pool",
+    // MAINNET
+    "waves4pool" : {
+        "name" : "waves4pool",
+        "full_name" : "waves4pool",
+        "symbol" : "waves4pool",
         "reference_asset" : "USD",
-        "pool_address" : "3N8p4dscxCN8XkfWopQP55a2AyvFD9cJx8a",
-        "gauge_address" : "3N8tLY9K2VpGVGcTT2sfyjz2QnhojAjf7WP",
-        "token_id" : "JAXQwrhuQk4tSkGjLB5H2Hn1PhvgNTTrDRaZqb5kotMN",
+        "pool_address" : "3P42DfhtdpyfzpqCEw2H1CvmgCRyrot64yi",
+        "gauge_address" : "3P4YACZAqdzFT1Q1dpmDvz3hHagJ6r9vRKf",
+        "token_id" : "JAb8w1ocdaYEEUDMBFmz27EmD2RStbYA6dDxvyvrzTHJ",
         "token_decimals" : 6,
-        "coins" : [ "USDN", "USDT", "USDC", "BUSD", "WDAI", "EAST" ],
+        "coins" : ["USDT", "USDC", "BUSD", "DAI"],
         "coin_ids" : [
-            "h67Gp9FQDa6tVFXbfhBrkKWzLvAygHk4sPG7WySHgnA",
-            "2byPKMNSe4QVuUm2CEPjcVkPSDxQysX5CaJQXxKnJZPb",
-            "E3HWLmLLNJ8vC3U3SPfxLdoBbzmnZQmToopicQwD91yN",
-            "9MZwABiVKKb1KebbJET7941wJqZs9hG8d9Reza1QTdt3",
-            "EDPRXujFFy5DJySGFbj8ckB9WCi9pF2rLqab66jrfvz",
-            "GjscxB4McTpst8SbhgFbY8UDMSdswuNmPYe4kTNsQjQb",
+            "34N9YcEETLWn93qYQ64EsP1x89tSruJU44RrEMSXXEPJ",
+            "6XtHjpXbs9RRJP2Sr9GUyVqzACcby9TkThHXnjVC5CDJ",
+            "8DLiYZjo3UUaRBTHU7Ayoqg4ihwb6YH1AfXrrhdjQ7K1",
+            "8zUYbdB8Q6mDhpcXYv52ji8ycfj4SDX4gJXS7YY3dA4R",
         ],
-        "decimals" : [ 6, 6, 6, 6, 6, 8 ]
-    }
+        "decimals" : [6, 6, 6, 6],
+    },
 
     // // TESTNET
     // "waves3pool" : {
@@ -29,37 +27,19 @@ export const POOLS_DATA: IDict<IPoolData> = {
     //     "full_name" : "waves3pool",
     //     "symbol" : "waves3pool",
     //     "reference_asset" : "USD",
-    //     "pool_address" : "3N6LyfP6LxBjzYtYf54srGheVuiSB9ttGUF",
-    //     "gauge_address" : "3N5d1PpYH9xHZPCxsbYU9kzHgoxbVn4Fauh",
-    //     "token_id" : "GHkh4rh4Spt1xFB5ABXj8RsjCMczNeNi6vbaKAGiQkm3",
+    //     "pool_address" : "3N8p4dscxCN8XkfWopQP55a2AyvFD9cJx8a",
+    //     "gauge_address" : "3N8tLY9K2VpGVGcTT2sfyjz2QnhojAjf7WP",
+    //     "token_id" : "JAXQwrhuQk4tSkGjLB5H2Hn1PhvgNTTrDRaZqb5kotMN",
     //     "token_decimals" : 6,
-    //     // "crv_id" : "HbyPRitPyShfSzMcD7wBCgrAdHVkXBDPZWuRrAGEA4y",
-    //     // "crv_decimals" : 8,
-    //     "coins" : [ "USDN", "USDT", "USDC" ],
+    //     "coins" : [ "USDN", "USDT", "USDC", "BUSD", "WDAI", "EAST" ],
     //     "coin_ids" : [
-    //         "8LhxCrVXyf3hNEKU7aLFg7bchrJ6Rv3tVU9GFVKCZCuS",
-    //         "Gw5aa4PHtKnV27ebRkUc2akWhEnNJfmrZ3TZg7YxoovP",
-    //         "9WGXmhryaCqfWZ3d2yhBGfXhZkDTQVqC9fDMdYJfmxZB",
+    //         "h67Gp9FQDa6tVFXbfhBrkKWzLvAygHk4sPG7WySHgnA",
+    //         "2byPKMNSe4QVuUm2CEPjcVkPSDxQysX5CaJQXxKnJZPb",
+    //         "E3HWLmLLNJ8vC3U3SPfxLdoBbzmnZQmToopicQwD91yN",
+    //         "9MZwABiVKKb1KebbJET7941wJqZs9hG8d9Reza1QTdt3",
+    //         "EDPRXujFFy5DJySGFbj8ckB9WCi9pF2rLqab66jrfvz",
+    //         "GjscxB4McTpst8SbhgFbY8UDMSdswuNmPYe4kTNsQjQb",
     //     ],
-    //     "decimals" : [ 6, 6, 6 ]
-    // }
-
-    // CUSTOM NETWORK
-    // "waves3pool" : {
-    //     "name" : "waves3pool",
-    //     "full_name" : "waves3pool",
-    //     "symbol" : "waves3pool",
-    //     "pool_address" : "3MNNRF9Twpu7BuHobGfsof1MF4oKrpam8FW",
-    //     "gauge_address" : "3MJdoPSoZ7kmkfY61pGBsCYEDdZ3ZeDPRnT",
-    //     "token_id" : "Asm98KTGH615QvbMrapKZzGLqp8yjMGCrwKReKbKmez3",
-    //     "token_decimals" : 6,
-    //     "reference_asset" : "USD",
-    //     "coins" : [ "USDN", "USDT", "USDC" ],
-    //     "coin_ids" : [
-    //         "5eg2eUvmgBftgsx3NZuCgk5q7195zx7VxNoNWatGS3HD",
-    //         "DJEYQHGZ2LZzr37a5NK7BrYY3XKdAVXAFvaML9SGV7Do",
-    //         "CPsgiCdE97iKU7zW9roH8C3mJ3yHMhGc7bkvTatp3R4L",
-    //     ],
-    //     "decimals" : [ 6, 6, 6 ]
+    //     "decimals" : [ 6, 6, 6, 6, 6, 8 ]
     // }
 }
